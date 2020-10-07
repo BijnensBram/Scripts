5715375bb94b4ead95165f7e5dd164e3ca1b95dc
BijnensBram <bram@bijnens.com>
2020-10-07 15:57:40 +0200

    adding wayland scripts and minor updates

--- randwallpaper_wayland.sh
+++ randwallpaper_wayland.sh
@@ -3,4 +3,9 @@ wallpaperdir="$HOME/Pictures/wallpaper"
 
 files=($wallpaperdir/*)
 
-randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"` && swaybg --mode=stretch -i $randompic
+if [ $XDG_SESSION_TYPE = "wayland" ];then
+	killall swaybg
+	randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"` && swaybg --mode=stretch -i $randompic
+else
+	randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"` && xwallpaper --zoom $randompic
+fi
